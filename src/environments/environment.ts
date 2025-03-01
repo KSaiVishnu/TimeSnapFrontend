3e96ef3d2d308f4ed1ffadaa416388902a695857
KSaiVishnu <svt20042005@gmail.com>
2025-03-01 16:31:58 +0530

    sixth commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-    apiBaseUrl: 'https://localhost:7062/api'
+    // apiBaseUrl: 'https://localhost:7062/api'
+    apiBaseUrl: 'https://timesnapprojectbackend-g9a6aya3g2gnhpgz.canadacentral-01.azurewebsites.net/api'
+
   };
   
