3af284c5580876c6b851d92dbea112ded370d21e
KSaiVishnu <svt20042005@gmail.com>
2025-04-23 08:31:35 +0530

    changes in account component

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   // apiBaseUrl: 'https://localhost:7062/api',
   // apiBaseUrl: 'https://timesnapprojectbackend-g9a6aya3g2gnhpgz.canadacentral-01.azurewebsites.net/api',
-  apiBaseUrl: 'https://localhost:7245/api',
-  // apiBaseUrl:"https://framsiktindiatools.azurewebsites.net/api",
+  // apiBaseUrl: 'https://localhost:7245/api',
+  apiBaseUrl:"https://framsiktindiatools.azurewebsites.net/api",
   // apiBaseUrl:
   //   'https://timesnapbackendmysql20250316232503-bjhda8gxgra6d4d3.canadacentral-01.azurewebsites.net/api',
   // apiBaseUrl: 'https://timesnapbackend-mysql-3.onrender.com/api',
-  googleClientId: '' // <-- This will be replaced by the actual value during deployment
+  googleClientId: '257848338595-hs3vnsefa2mpgq2esjk6clpdq8pu062v.apps.googleusercontent.com' // <-- This will be replaced by the actual value during deployment
 };
